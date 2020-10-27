cc533260219a8e41bdf4ad7f3eac92ff1d000b02
LeoZhang553 <leoz553@deltalearns.ca>
2020-10-27 13:02:31 -0700

    d

---
+++ check.py
@@ -0,0 +1,36 @@
+ ["English","Math","PE","Computers","History","Biology","Japanese"]
+  [91, 94, 87, 99, 82, 100, 73]
+
+      st2 = student("Joe Lunchbox","12346", 11)
+    st1.getCourses( ["English","Math","Physics","Computers","Geography","Chemistry","French"] )
+    st1.getGrades( [71, 98, 93, 95, 68, 81, 71])
+
+    a1=input("course1 = ")
+        a2=input("course2 = ")
+        a3=input("course3 = ")
+        a4=input("course4 = ")
+        a5=input("course5 = ")
+        a6=input("course6 = ")
+        a7=input("course7 = ")
+        a8=input("course8 = ")
+
+        course=[]
+
+        if a1 != "none":
+            course.append(a1)
+        if a2 != "none":
+            course.append(a2)
+        if a3 != "none":
+            course.append(a3)
+        if a4 != "none":
+            course.append(a4)
+        if a5 != "none":
+            course.append(a5)
+        if a6 != "none":
+            course.append(a6)
+        if a7 != "none":
+            course.append(a7)
+        if a8 != "none":
+            course.append(a8)
+
+        return course
--- task1.py
+++ task1.py
@@ -27,69 +27,52 @@ class student:
     name=''
     number=''
     grade=0
-    course=[]
-    grades=[]
 
-    def __init__(self,name,number,grade,course,grades):
+
+    def __init__(self,name,number,grade):
         # You will need to create your own input parameters for all methods
         self.name=name
         self.number=number
         self.grade=grade
-        self.course=course
-        self.grades=grades
 
         print('Student name is '+self.name)
         print('His/her student number is '+self.number)
 
-    def getCourses(self):
+    def inputCourses(self):
 
         print("enter your course grades")
-        print("enter none if you do not have any more courses")
         print("===============================================")
 
-        a1=input("course1 = ")
-        a2=input("course2 = ")
-        a3=input("course3 = ")
-        a4=input("course4 = ")
-        a5=input("course5 = ")
-        a6=input("course6 = ")
-        a7=input("course7 = ")
-        a8=input("course8 = ")
+        course=[]
+        i=0
 
-        if a1 != "none":
-            course.append(a1)
-        if a2 != "none":
-            course.append(a2)
-        if a3 != "none":
-            course.append(a3)
-        if a4 != "none":
-            course.append(a4)
-        if a5 != "none":
-            course.append(a5)
-        if a6 != "none":
-            course.append(a6)
-        if a7 != "none":
-            course.append(a7)
-        if a8 != "none":
-            course.append(a8)
+        while i<7:
+            a=input("course = ")
+            if a != "0":
+                course.append(a)
+            i+=1
 
         return course
 
+    def getCourses(self,course):
+        self.course=course
 
-    def getGrades(self,course):
-
+    def inputGrades(self,course):
         num=len(course)
-        num=int(num)
         i=0
         print("enter your course grades")
         print("===============================================")
+        grades=[]
         while i<num:
-        a=input("course grade= ")
-        a=int(a)
-        grades.append(a)
-        i += 1
+            a=input("course grade= ")
+            a=int(a)
+            grades.append(a)
+            i += 1
 
         return grades
+
+    def getGrades(self,grades):
+        self.grades=grades
 
     def average(self,grades):
         num=len(grades)
@@ -99,13 +82,16 @@ class student:
         while i<num:
             a += grades[i]
             i += 1
-        average=a/8
+        average=a/num
+        average=str(average)
+        print("average= "+average)
         return average
 
     def getHonorRoll(self,grades):
         grades.sort()
         ave5=(grades[-1]+grades[-2]+grades[-3]+grades[-4]+grades[-5])/5
         if ave5 >= 86:
+            print("Honor Roll")
             return True
         else:
             return False
@@ -119,7 +105,7 @@ class student:
         print(course[index])
         print(grades[index])
 
-    def __del__():
+    def __del__(self):
         print("END")
 
 
@@ -128,15 +114,13 @@ def main():
     # do not modify this function
 
     st1 = student("Anita Bath","91334",11)
-    st1.getCourses( ["English","Math","PE","Computers","History","Biology","Japanese"] )
-    st1.getGrades( 91, 94, 87, 99, 82, 100, 73)
+    course=st1.inputCourses()
+    grades=st1.inputGrades(course)
+    st1.getCourses(course)
+    st1.getGrades(grades)
+    st1.average(grades)
+    st1.getHonorRoll(grades)
+    st1.showCourses(course)
+    st1.showGrades(course,grades)
 
-    st2 = student("Joe Lunchbox","12346", 11)
-    st1.getCourses( ["English","Math","Physics","Computers","Geography","Chemistry","French"] )
-    st1.getGrades( 71, 98, 93, 95, 68, 81, 71)
-
-
-
-grades=getGrades
-course=getCourse
 main()
